7c6e696bc0211ead0d0d22e17722c880e2c9840b
epdejager <eliza.dejager@statpro.com>
2012-07-27 10:49:19 +0200

    merge complete

---
+++ app/assets/javascripts/directupload.js
@@ -0,0 +1,44 @@
+function directUpload() {
+  console.log('In directUpload')
+  $('#fileupload').fileupload({
+    forceIframeTransport: true,    // VERY IMPORTANT.  you will get 405 Method Not Allowed if you don't add this.
+    autoUpload: true,
+    add: function (event, data) {
+      console.info('In the add function');
+      $.ajax({
+        url: "/directimport",
+        type: 'POST',
+        dataType: 'json',
+        data: {doc: {title: data.files[0].name}},
+        async: false,
+        success: function(retdata) {
+          // after we created our document in rails, it is going to send back JSON of they key,
+          // policy, and signature.  We will put these into our form before it gets submitted to amazon.
+          console.info('In the success callback');
+          $('#fileupload').find('input[name=key]').val(retdata.key);
+          $('#fileupload').find('input[name=policy]').val(retdata.policy);
+          $('#fileupload').find('input[name=signature]').val(retdata.signature);          
+        }
+        
+      });
+
+      data.submit();
+    },
+    send: function(e, data) {
+      // show a loading spinner because now the form will be submitted to amazon, 
+      // and the file will be directly uploaded there, via an iframe in the background. 
+      $('#loading').show();
+    },
+    fail: function(e, data) {
+      console.log('fail');
+      console.log(data);
+    },
+    done: function (event, data) {
+      // here you can perform an ajax call to get your documents to display on the screen.
+      //$('#your_documents').load("/documents?for_item=1234");
+      
+      // hide the loading spinner that we turned on earlier.
+      $j('#loading').hide();
+    },
+  });
+}
